c5d0a7c6755a6901c6c7cf12533a319b4b87128e
Johannes Gårdsted Valbjørn <sloev@users.noreply.github.com>
2017-02-13 15:05:11 +0100

    bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='lambdarest',
-    version='0.0.6',
+    version='0.0.7',
     description="pico framework for aws lambda with optional json schema validation",
     long_description=readme + '\n\n' + history,
     author="jgv",
