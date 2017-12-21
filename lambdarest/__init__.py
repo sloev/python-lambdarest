2a2edc8176c2724f6ee8676dc1b51c27eb16b69f
Patrick-Ranjit D. Madsen <paddie@users.noreply.github.com>
2017-12-21 16:03:24 +0100

    set body to None, do not marshal into 'null'
    ```
    >>> json.dumps(None)
    'null'
    ```

--- lambdarest/__init__.py
+++ lambdarest/__init__.py
@@ -30,7 +30,7 @@ class Response(object):
 
     def to_json(self):
         return {
-            "body": json.dumps(self.body or None),
+            "body": json.dumps(self.body) if self.body else None,
             "statusCode": self.status_code or 200,
             "headers": self.headers or {}
         }
